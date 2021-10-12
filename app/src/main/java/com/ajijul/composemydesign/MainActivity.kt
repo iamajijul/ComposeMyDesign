342165107aecc20783a66b5d7aa152a85f6233f5
iamajijul <ajijumondal786@gmail.com>
2021-10-12 20:53:13 +0400

    first commit

---
+++ app/src/main/java/com/ajijul/composemydesign/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.ajijul.composemydesign
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.ajijul.composemydesign.ui.theme.ComposeMyDesignTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+                    Greeting("Ajijul")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+        Greeting("Android")
+}
