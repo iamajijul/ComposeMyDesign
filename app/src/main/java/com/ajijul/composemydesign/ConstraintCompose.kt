95660d2a15c8fa85ac83def03d67c25df6feedff
iamajijul <ajijumondal786@gmail.com>
2021-11-15 21:43:12 +0400

    decoupled constraint

--- app/src/main/java/com/ajijul/composemydesign/ConstraintCompose.kt
+++ app/src/main/java/com/ajijul/composemydesign/ConstraintCompose.kt
@@ -1,6 +1,7 @@
 package com.ajijul.composemydesign
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
@@ -8,28 +9,55 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 
 @Composable
 fun LoaderWithText(text: String) {
-    ConstraintLayout(modifier = Modifier.fillMaxSize().background(Color.Cyan)) {
-        val (progressBar1, text1) = createRefs()
-        val topGuideline = createGuidelineFromTop(0.2f)
-        val bottomGuideline = createGuidelineFromBottom(0.2f)
-        CircularProgressIndicator(modifier = Modifier.constrainAs(progressBar1){
-            top.linkTo(topGuideline)
+    BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+        val constraint =
+            if (minWidth < 600.dp) myDecoupledConstraint(0.5f) else myDecoupledConstraint(0.5f)
+        ConstraintLayout(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Cyan),
+            constraintSet = constraint
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier.layoutId("progressBarID"),
+                color = MaterialTheme.colors.primary
+            )
+            Text(
+                text = text,
+                modifier = Modifier.layoutId("textID"),
+                style = TextStyle(fontSize = 20.sp, fontStyle = FontStyle.Italic)
+            )
+        }
+    }
+
+}
+
+private fun myDecoupledConstraint(verticalBias: Float): ConstraintSet {
+    return ConstraintSet {
+        val topGuideLine = createGuidelineFromTop(verticalBias)
+        val progressBar = createRefFor("progressBarID")
+        val text = createRefFor("textID")
+        constrain(progressBar) {
+            top.linkTo(topGuideLine)
+            bottom.linkTo(topGuideLine)
             start.linkTo(parent.start)
             end.linkTo(parent.end)
-        },
-        color = MaterialTheme.colors.primary
-        )
-        Text(text = text,modifier = Modifier.constrainAs(text1){
-            bottom.linkTo(bottomGuideline)
-            start.linkTo(progressBar1.start)
-            end.linkTo(progressBar1.end)
-        },style = TextStyle(fontSize = 20.sp,fontStyle = FontStyle.Italic))
+        }
+
+        constrain(text) {
+            top.linkTo(progressBar.bottom)
+            start.linkTo(progressBar.start)
+            end.linkTo(progressBar.end)
+        }
     }
 }
