3a038a4097ff7d7dc2b48bd181fc70a3dffa908e
iamajijul <ajijumondal786@gmail.com>
2021-11-15 21:17:35 +0400

    Constraint with guideline

--- app/src/main/java/com/ajijul/composemydesign/ConstraintCompose.kt
+++ app/src/main/java/com/ajijul/composemydesign/ConstraintCompose.kt
@@ -1,29 +1,35 @@
 package com.ajijul.composemydesign
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
 fun LoaderWithText(text: String) {
-    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+    ConstraintLayout(modifier = Modifier.fillMaxSize().background(Color.Cyan)) {
         val (progressBar1, text1) = createRefs()
+        val topGuideline = createGuidelineFromTop(0.2f)
+        val bottomGuideline = createGuidelineFromBottom(0.2f)
         CircularProgressIndicator(modifier = Modifier.constrainAs(progressBar1){
-            top.linkTo(parent.top)
-            bottom.linkTo(parent.bottom)
+            top.linkTo(topGuideline)
             start.linkTo(parent.start)
             end.linkTo(parent.end)
         },
         color = MaterialTheme.colors.primary
         )
         Text(text = text,modifier = Modifier.constrainAs(text1){
-            top.linkTo(progressBar1.bottom)
+            bottom.linkTo(bottomGuideline)
             start.linkTo(progressBar1.start)
             end.linkTo(progressBar1.end)
-        })
+        },style = TextStyle(fontSize = 20.sp,fontStyle = FontStyle.Italic))
     }
 }
